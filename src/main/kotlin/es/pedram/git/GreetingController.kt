c0af3aa0f95b78eeee00c0ef976660f1467d07fa
Pedram Hajesmaeeli <pedram.hajesmaeeli@glovoapp.com>
2023-01-08 18:02:04 +0100

    Change foo to bar

--- src/main/kotlin/es/pedram/git/GreetingController.kt
+++ src/main/kotlin/es/pedram/git/GreetingController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class GreetingController {
 
-    // foo
+    // bar
     @GetMapping
     fun greeting(@RequestParam name: String): String {
         return "Hello $name"
