c1c4d40a0c0bcfa8637da734ff2b1bae7ef241b1
Pedram Hajesmaeeli <pedram.hajesmaeeli@glovoapp.com>
2023-01-08 17:43:33 +0100

    Add name to greeting

--- src/main/kotlin/es/pedram/git/GreetingController.kt
+++ src/main/kotlin/es/pedram/git/GreetingController.kt
@@ -1,13 +1,14 @@
 package es.pedram.git
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class GreetingController {
 
     @GetMapping
-    fun greeting(): String {
-        return "Hello world"
+    fun greeting(@RequestParam name: String): String {
+        return "Hello $name"
     }
 }
